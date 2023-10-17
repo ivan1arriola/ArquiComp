6d646d64489e7d8f65f987632f2418db56a18f42
Ivan Arriola <ivan.arriola@fing.edu.uy>
2023-10-17 16:44:35 -0300

    primer bosquejo

---
+++ obligatorio.c
@@ -0,0 +1,28 @@
+#define PUERTO_ENTRADA equ 20
+#define PUERTO_SALIDA equ 21
+#define PUERTO_LOG equ 22
+#define AREA_DE_MEMORIA equ 2048
+
+
+/*
+Codificacion de comandos
+
+Comando             Parametros         Codigo   
+Cambiar Modo        Modo               1         
+Agregar Nodo        Numero             2         
+Calcular Altura     -                  3        
+Calcular Suma       -                  4         
+Imprimir arbol      Orden              5          
+Imprimir Memoria    N                  6          
+Detener Programa    -                  255         
+
+*/
+
+
+
+
+
+int main() {
+    return 0;
+}
+
