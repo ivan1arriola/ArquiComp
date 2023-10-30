a6cc3eed5f917a3635cf71612cf2542c0ad5b8a2
Ivan Arriola <ivan.arriola@fing.edu.uy>
2023-10-29 23:24:59 -0300

    asm

---
+++ obligatorio.asm
@@ -0,0 +1,21 @@
+.data  ; Segmento de datos
+
+.code  ; Segmento de código
+
+
+
+
+
+
+
+
+.ports ; Definición de puertos
+20; Puerto ENTRADA
+21; PUERTO SALIDA
+22; PUERTO LOG
+
+.interrupts ; Manejadores de interrupciones
+; Ejemplo interrupcion del timer
+;!INT 8 1
+;  iret
+;!ENDINT
