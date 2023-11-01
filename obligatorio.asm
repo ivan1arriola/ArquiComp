110f1ae6448ab3ec127b17720eca7eeb09dd4b5b
Ivan Arriola <ivan.arriola@fing.edu.uy>
2023-11-01 17:51:44 -0300

    agregar nodos estatico funciona aparentemente

--- obligatorio.asm
+++ obligatorio.asm
@@ -1,8 +1,7 @@
 ; ---------- SEGMENTO DE DATOS ----------
 
 .data  
-; En el segmento ES va a estar el arbol
-#define ES 7000h
+
 modo db 0 ; modo inicial estatico = 0, dinamico = 1
 raiz dw 0 ; raiz del arbol
 
@@ -13,6 +12,10 @@ raiz dw 0 ; raiz del arbol
 
 ; Constantes
 VACIO equ 0x8000
+
+; En el segmento ES va a estar la raiz del arbol
+#define ES 7000h
+;SEGMENTO_ARBOL equ 7000h
 
 
 ; Puertos
@@ -80,33 +83,38 @@ AREA_MEMORIA equ 2048
 
 .code
 
+; Inicializa la memoria del arbol
 reiniciarMemoria:
-; Aqui se tiene que iniciarlizar la memoria //TODO: Aun no hace nada realmente
-mov ax, ES
-mov es, ax
-mov cx, AREA_MEMORIA
-mov ax, VACIO
-mov di, 0
-ciclo_inicializacion:
-    mov es:[di], ax
-    add di, 2
-    
+    mov ax, VACIO         ; Valor a inicializar en la memoria
+    mov di, 0             ; Inicializar el registro de �ndice DI a 0
+	mov cx, AREA_MEMORIA
+
+inicializar_bucle:
+    mov ES:[di], ax       ; Almacena el valor de AX en la direcci�n de memoria apuntada por ES:DI
+    add di, 2             ; Avanza al siguiente espacio de memoria (2 bytes por palabra)
+    dec cx                ; Decrementa el contador
+    jnz inicializar_bucle ; Salta de nuevo al bucle si CX no ha llegado a cero
+
+
 
 comienzoWhile:
-mov ax, CODIGO_BITACORA; cargar en AX el codigo de bitacora
-out PUERTO_LOG, ax; escribir_puerto(PUERTO_LOG, CODIGO_BITACORA);
+    mov ax, CODIGO_BITACORA; cargar en AX el codigo de bitacora
+    out PUERTO_LOG, ax; imprime en LOG el codigo de bitacora
 
-in ax, PUERTO_ENTRADA ; comando = leer_puerto_entrada("Ingrese un comando:");
-out PUERTO_LOG, ax; escribir_puerto(PUERTO_LOG, comando);
+    in ax, PUERTO_ENTRADA ; almacena en AX el comando 
+    out PUERTO_LOG, ax; escribe el comando por LOG
 
+	; Busca si algun comando coincide y redirije hacia ahi, en otro casi imprime error
+    cmp ax, CAMBIAR_MODO
+    je cambiarModo
 
-cmp ax, CAMBIAR_MODO
-je cambiarModo
+	cmp ax, AGREGAR_NODO
+    je agregarNodo
 
-cmp ax, DETENER_PROGRAMA
-je detenerPrograma
+    cmp ax, DETENER_PROGRAMA
+    je detenerPrograma
 
-jmp comandoIncorrecto; si llega aca es porque no era un comando valido
+    jmp comandoIncorrecto; si llega aca es porque no era un comando valido
 
 
 
@@ -130,7 +138,60 @@ cambiarModo:
 	
     jmp reiniciarMemoria ; Salta a "reiniciarMemoria"
 
+agregarNodo:
+    in ax, PUERTO_ENTRADA ; lee el valor del nodo de la entrada
+	out PUERTO_LOG, AX; imprime el valor en el puerto log
 
+
+    cmp BX, 0 ; Si BX es 0, se accede al modo estatico
+    jmp agregarNodoModoEstatico
+
+    cmp BX, 1 ; Si BX es 1, se accede al modo dinamico
+    jmp agregarNodoModoDinamico
+
+
+agregarNodoModoEstatico:
+    mov si, 0  ; Inicializa el índice en 0
+    whileAgregarNodoEstatico:
+        cmp si, AREA_MEMORIA  ; Compara si hemos llegado al final del área de memoria
+        jae errorEscribirFueraDeArea ; Si es así, salta al manejo de error (fuera de área)
+
+        mov CX, ES:[si]  ; Carga el valor en la dirección de memoria apuntada por ES:SI en CX
+        cmp CX, VACIO  ; Compara si el valor es VACIO
+        je nodoVacioEncontrado ; Si es VACIO, se ha encontrado un lugar para el nodo
+
+        cmp CX, AX  ; Compara el valor actual con el valor a agregar (CX y AX)
+        je nodoDuplicado ; Si son iguales, el nodo ya existe
+        jl moverIzquierda ; Si el valor actual es menor que el valor a agregar, muévete a la izquierda
+        jge moverDerecha ; Si es mayor o igual, muévete a la derecha
+
+        nodoVacioEncontrado:
+            mov ES:[si], ax ; Almacena el valor en la dirección de memoria apuntada por ES:SI
+            mov ax, CODIGO_EXITO ; Código de éxito
+            out PUERTO_LOG, ax ; Imprime el código de éxito en el puerto log
+            jmp comienzoWhile
+        nodoDuplicado:
+            jmp errorNodoYaExiste ; Salta al manejo de error (nodo duplicado)
+        moverIzquierda:
+            shl si, 1  ; Mueve a la izquierda (multiplica por 2)
+            inc si  ; Agrega 1 para ir al hijo izquierdo
+            jmp whileAgregarNodoEstatico ; Vuelve al bucle principal
+        moverDerecha:
+            shl si, 1  ; Mueve a la derecha (multiplica por 2)
+            add si, 2  ; Agrega 2 para ir al hijo derecho
+            jmp whileAgregarNodoEstatico ; Vuelve al bucle principal
+
+
+agregarNodoModoDinamico:
+; TODO
+	jmp comienzoWhile ; Salta al main nuevamente
+
+
+
+
+
+
+	
 
 
 
@@ -140,28 +201,30 @@ cambiarModo:
 ;
 
 
-; Inicializa la memoria del arbol
-inicializar_memoria:
-; el arbol tiene 2048 bytes de memoria
-; todos los espacio de memoria estan inicializados en 0x8000
-; el primer espacio de memoria es la raiz del arbol independientemente del modo
 
 
 
-;ret
+
 
 
 
 
 parametroIncorrecto:
-mov AX, CODIGO_PARAMETRO_INVALIDO
-out PUERTO_LOG, ax 
-jmp comienzoWhile
-
+    mov AX, CODIGO_PARAMETRO_INVALIDO
+    out PUERTO_LOG, ax 
+    jmp comienzoWhile
 comandoIncorrecto:
-mov AX, CODIGO_COMANDO_INVALIDO
-out PUERTO_LOG, ax 
-jmp comienzoWhile
+    mov AX, CODIGO_COMANDO_INVALIDO
+    out PUERTO_LOG, ax 
+    jmp comienzoWhile
+errorNodoYaExiste:
+    mov ax, CODIGO_NODO_YA_EXISTE
+    out PUERTO_LOG, ax
+    jmp comienzoWhile
+errorEscribirFueraDeArea:
+    mov AX, CODIGO_ESCRIBIR_FUERA_DE_AREA
+    out PUERTO_LOG, ax 
+    jmp comienzoWhile
 
 
 
@@ -176,7 +239,7 @@ detenerPrograma:
 
 ; ---------- SEGMENTO DE PUERTOS ----------
 .ports
-20: 1, 0, 1, 1, 255
+20: 1,0,2,5,2,-1,2,5,2,7,2,8,2,9,2,10,2,11,2,12,2,13,2,14,2,15,2,16,2,17,2,18,255
 
 
 
